2dc933dcc6920a8915da6d03f54024b1aac6dd3f
Noison <noise1995@inbox.ru>
2022-09-09 21:24:18 +0600

    ValidateSubsequence solved

--- src/ValidateSubsequence.java
+++ src/ValidateSubsequence.java
@@ -1,2 +1,27 @@
+import java.util.List;
+
 public class ValidateSubsequence {
+
+    public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+        if (sequence.size() > array.size()) {
+            return false;
+        }
+
+        int validCount = 0;
+        int seqMarker = 0;
+
+        for (Integer arrNum : array) {
+            if (arrNum.equals(sequence.get(seqMarker))) {
+                seqMarker++;
+                validCount++;
+            }
+
+            if (validCount == sequence.size()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
