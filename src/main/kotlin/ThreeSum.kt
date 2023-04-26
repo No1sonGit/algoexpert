228163bfb9272c2fe58b514e3bfdcd6ff9c61743
Noison <noise1995@inbox.ru>
2023-04-26 19:16:57 +0600

    three sum

--- src/main/kotlin/ThreeSum.kt
+++ src/main/kotlin/ThreeSum.kt
@@ -1,5 +1,19 @@
 fun threeNumberSum(array: MutableList<Int>, targetSum: Int): List<List<Int>> {
+    val result = ArrayList<List<Int>>()
 
+    for ((index, i) in array.withIndex()) {
+        val cache = HashSet<Int>()
 
-    return listOf<List<Int>>()
+        for (j in index + 1 until array.size) {
+            val needed = targetSum - i - array[j];
+
+            if (cache.contains(needed)) {
+                result.add(listOf(needed, array[j], i).sorted())
+            } else {
+                cache.add(array[j])
+            }
+        }
+    }
+
+    return result.sortedWith(compareBy<List<Int>> { it[0] }.thenBy { it[1] }.thenBy { it[2] })
 }
